c4f6fe6009a467550490e8afe6f4880c71914821
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-12 17:15:18 +0530

    Commit with Initial Project

---
+++ src/test/java/com/yash/dev/auth/SpringBootAuthDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.dev.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
