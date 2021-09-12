1ddf95e6bda49e3ea6ce3469364248eefd5f60f4
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-12 17:20:28 +0530

    Commit with DB Authentication Logic

---
+++ src/main/java/com/yash/dev/auth/model/Role.java
@@ -0,0 +1,39 @@
+package com.yash.dev.auth.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Yashwanth Anand S
+ **/
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int roleId;
+
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/yash/dev/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.yash.dev.auth.service;
+
+import com.yash.dev.auth.model.CustomUserDetails;
+import com.yash.dev.auth.model.Users;
+import com.yash.dev.auth.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * Created by Yashwanth Anand S
+ **/
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> optionalUsers = usersRepository.findByName(username);
+
+        optionalUsers
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUsers
+                .map(CustomUserDetails::new).get();
+    }
+}
